735d6dce98db85d17b512600d11a834f92c103ab
Greg Cuesta <gregory.cuesta@gmail.com>
2025-02-04 12:39:47 -0800

    Blog: Latest posts section

--- services/client/app/blog/page.tsx
+++ services/client/app/blog/page.tsx
@@ -1,12 +1,69 @@
 import { Card, CardContent } from '@/components/core-ui/card'
 import BlogFeatured from '@/components/blog/Featured'
+import BlogArticleCard from '@/components/blog/ArticleCard'
+
+const posts = [
+  {
+    id: 1,
+    title: 'My First Post',
+    description: 'This is the description of my first post. It is a long description that is a bit longer than the others.',
+    slug: 'my-first-post',
+    image: '/images/generated/fam-4.webp',
+    author: 'Greg Cuesta',
+    date: 'Feb 5, 2025',
+  },
+  {
+    id: 2,
+    title: 'My Second Post',
+    description: 'This is the description of my second post',
+    slug: 'my-second-post',
+    image: '/images/generated/fam-4.webp',
+    author: 'Greg Cuesta',
+    date: 'Feb 5, 2025',
+  },
+  {
+    id: 3,
+    title: 'My Third Post',
+    description: 'This is the description of my third post',
+    slug: 'my-third-post',
+    image: '/images/generated/fam-4.webp',
+    author: 'Greg Cuesta',
+    date: 'Feb 5, 2025',
+  },
+  {
+    id: 4,
+    title: 'My Fourth Post',
+    description: 'This is the description of my fourth post',
+    slug: 'my-fourth-post',
+    image: '/images/generated/fam-4.webp',
+    author: 'Greg Cuesta',
+    date: 'Feb 5, 2025',
+  },
+  {
+    id: 5,
+    title: 'My Fifth Post',
+    description: 'This is the description of my fifth post',
+    slug: 'my-fifth-post',
+    image: '/images/generated/fam-4.webp',
+    author: 'Greg Cuesta',
+    date: 'Feb 5, 2025',
+  },
+]
+
 export default function Blog() {
   return (
-    <div className="mx-auto pt-24 px-4">
-      <div className="max-w-[1200px] mx-auto mb-14">
+    <div className="mx-auto pt-24 p-8 px-4 max-w-[1200px] mx-auto">
+      <div className="mb-14">
         <BlogFeatured />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4"></div>
+      <div>
+        <h2 className="text-center mb-8">Latest Posts</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-4 lg:gap-6">
+          {posts.map((post) => (
+            <BlogArticleCard key={post.id} post={post} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
---
+++ services/client/components/blog/ArticleCard.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+export default function BlogArticleCard({ post }: { post: { id: number, title: string, slug: string, description: string, image: string, author: string, date: string } }) {
+    return (
+        <Link href={`/blog/${post.slug}`}>
+        <div className="bg-white shadow-lg rounded-lg rounded-t-lg hover:scale-105 transition-all duration-300 pb-2 h-full">
+
+            <Image src={post.image} alt={post.title} width={300} height={300} className="w-full rounded-t-lg" />
+            <div className="p-4">
+            <div className="text-sm lg:text-xs text-gray-500 flex justify-between items-center">
+                <p>{post.author}</p>
+                <p>{post.date}</p>
+            </div>
+                <h3 className="text-tertiary text-lg font-bold">{post.title}</h3>
+                <p className="text-gray-600">{post.description}</p>
+            </div>
+        </div>
+        </Link>
+    )
+}
