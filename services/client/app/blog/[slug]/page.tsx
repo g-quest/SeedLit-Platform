bf8f507b420670e74bfb1df7a6f07d3a35c18c33
Greg Cuesta <gregory.cuesta@gmail.com>
2025-02-04 11:57:23 -0800

    Blog: Typing fixes

--- services/client/app/blog/[slug]/page.tsx
+++ services/client/app/blog/[slug]/page.tsx
@@ -2,9 +2,9 @@ import { notFound } from 'next/navigation'
 
 // You don't need to manually get the slug - Next.js provides it automatically
 interface PageProps {
-  params: {
+  params: Promise<{
     slug: string  // Will automatically contain the URL segment
-  }
+  }>
 }
 
 // Function to validate if slug exists
@@ -16,19 +16,18 @@ async function validateSlug(slug: string): Promise<boolean> {
 }
 
 export default async function BlogPost({ params }: PageProps) {
-  const slugExists = await validateSlug(params.slug)
+  const { slug } = await params; // Await the promise to get the slug
+  const slugExists = await validateSlug(slug);
   
   if (!slugExists) {
     notFound() // This will show the closest not-found.tsx page
   }
 
-  // params.slug will be whatever is in the URL
-  // e.g., for /blog/my-first-post, params.slug = "my-first-post"
-  console.log('Current slug:', params.slug)
+  console.log('Current slug:', slug);
   
   return (
     <div>
-      <h1>Blog Post: {params.slug}</h1>
+      <h1>Blog Post: {slug}</h1>
     </div>
-  )
+  );
 }
