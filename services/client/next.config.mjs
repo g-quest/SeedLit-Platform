5b8b0449cb796a1b49d138018b115ac5114793e9
Greg Cuesta <gregory.cuesta@gmail.com>
2025-02-12 18:57:11 -0800

    Next images domain change test

--- services/client/next.config.mjs
+++ services/client/next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // images: {
+  //   domains: ['images.ctfassets.net'],
+  // },
   images: {
-    domains: ['images.ctfassets.net'],
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+      },
+    ],
   },
 }
 
